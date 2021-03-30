458e5c86dfd3d30ca5218a644882097bad8ca285
Darren Lim <darrenlimchuans@gmail.com>
2021-03-30 18:12:23 +0800

    Initial Commit with Header and Card

---
+++ common/Template.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Template = props => {
+    return (
+        <View>
+            <Text>
+                Template
+            </Text>
+        </View>
+    );
+};
+
+// Component StyleSheet
+const styles = StyleSheet.create({
+
+});
+
+// Export the Component
+export default Template;
---
+++ screen/StartGameScreen.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+import Card from '../common/Card';
+
+const StartGameScreen = props => {
+    return (
+        <View style={styles.screen}>
+            <Text style={styles.title}>Start a New Game!</Text>
+            <Card style={styles.inputContainer}>
+                <Text>Select a Number</Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <Button title="Reset" onPress={() => { }} />
+                    <Button title="Confirm" onPress={() => { }} />
+                </View>
+            </Card>
+        </View>
+    );
+};
+
+// Component StyleSheet
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        backgroundColor: 'white',
+        elevation: 5,
+        padding: 20,
+        borderRadius: 10
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    }
+});
+
+// Export the Component
+export default StartGameScreen;
